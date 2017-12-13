d062c7ffb9f6ebfd5389795d5e52fd6627fc9cbc
Wan-Ting Huang <103703004@nccu.edu.tw>
2017-12-13 14:17:18 +0800

    unity

---
+++ face/Assets/script/loadImg.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadImg : MonoBehaviour {
+
+    private GameObject eye;
+    private GameObject m_Image;
+    public Sprite eye1;
+
+    // Use this for initialization
+    void Start () {
+        eye = GameObject.Find("/Canvas/eye");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        eye.GetComponent<Image>().sprite = eye1;
+    }
+}
