a8c69dd0ef3df42fa467d87bef0f80bae3625101
jacky841114 <jacky841114@gmail.com>
2017-12-17 22:59:54 +0800

    test random and left to right

---
+++ face/Assets/script/Random_move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Random_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 temp = new Vector3(Random.Range(0.6f, 1000.6f), Random.Range(0.6f, 1000.6f), 0);
+        transform.position = temp;
+	}
+
+    private static float Range(float p1, float p2)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ face/Assets/script/transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transaction : MonoBehaviour
+{
+   
+    public float speed = 0.001f;//移動速度
+ 
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = new Vector3(0, 100, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        this.transform.position+=new Vector3(speed*Time.deltaTime,0,0);
+    }
+}
